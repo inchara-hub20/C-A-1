14e4ed51300f90224449c4609460cbc49b276b54
inchara <inchara.m@s.amity.edu>
2025-03-25 17:28:37 +0530

    third commit

--- L3/cprogram.c
+++ L3/cprogram.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
  void main () {
          int n;
-         printf("1.B.tech CSE\n2.B.tech AIML");
+         printf("1.B.tech CSE\n2.B.tech AIML\n3.B.tech ECE");
          if(n==1)
          {
                  printf("you have choosen B.tech CSE");
          }
-	 if(n==2)
-	 {
-		 printf("you have choosen B.tech AIML");
-	 }
+         if(n==2)
+         {
+                 printf("you have choosen B.tech AIML");
+         }
+         if(n==3)
+         {
+                 printf("you have choosen B.tech ECE");
+         }
  }
+
